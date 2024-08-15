239f9fdb6e4073c5ad253d46ba7673251e270383
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-08-15 13:41:54 +0100

    wip

--- python-impl/setup.py
+++ python-impl/setup.py
@@ -1,39 +1,14 @@
-import sys
-
 import setuptools
-
-
-requires = [
-    'base58',
-    'binary-helpers',
-    'blspy @ git+https://github.com/ArkEcosystem/bls-signatures@e7942654c48e94e955c81e7a131bc3d5e9d6b707#egg=blspy',
-    'coincurve',
-    'pycryptodomex',
-    'btclib',
-    'cryptography',
-]
-
-tests_require = [
-    'flake8>=3.5.0',
-    'flake8-import-order>=0.17.1',
-    'flake8-print>=3.1.0',
-    'flake8-quotes>=1.0.0',
-    'pytest>=3.6.1',
-    'pytest-cov>=2.5.1',
-]
-
-extras_require = {
-    'test': tests_require,
-    'dev': requires + tests_require
-}
-
-setup_requires = ['pytest-runner'] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
+from pathlib import Path
 
 setuptools.setup(
-    name="blspy",
+    name="blspy_impl",
     author="Mariano Sorgente",
     author_email="mariano@chia.net",
-    description="BLS signatures in c++ (python bindings)",
+    description="BLS signatures in c++ (pure python implementation)",
     python_requires=">=3.7",
     url='https://github.com/Chia-Network/bls-signatures',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    py_modules=[],
 )
